1010387a3393fcf197153c6a89b9b7f531a1b8ab
Joona Piirainen <joona.piirainen@gmail.com>
2021-06-14 20:48:01 +0300

    init

---
+++ apps/server/src/config.ts
@@ -0,0 +1,5 @@
+import dotenv from 'dotenv'
+dotenv.config()
+const { HOST, PORT } = process.env
+
+export { HOST, PORT }
--- apps/server/src/start-server.ts
+++ apps/server/src/start-server.ts
@@ -5,6 +5,7 @@ import { PrismaClient } from '@prisma/client'
 
 import typeDefs from './graphql/schema'
 import resolvers from './graphql/resolvers'
+import { HOST, PORT } from './config'
 
 dotenv.config()
 
@@ -15,4 +16,9 @@ const server = new ApolloServer({
    resolvers,
 })
 
-server.listen().then(({ url }) => console.log(`Server listening on: ${url}`))
+console.log(HOST)
+console.log(PORT)
+
+server
+   .listen({ port: PORT, host: HOST })
+   .then(({ url }) => console.log(`Server listening on: ${url}`))
